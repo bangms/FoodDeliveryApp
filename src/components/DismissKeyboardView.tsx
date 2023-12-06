12c3d631e95351e27370565159543e5bc3a1a917
bangms <cgpgbyj23@gmail.com>
2023-12-06 18:23:23 +0900

    HOC 제외 아이폰, 안드로이드 분기처리

--- src/components/DismissKeyboardView.tsx
+++ src/components/DismissKeyboardView.tsx
@@ -5,19 +5,27 @@ import {
   StyleProp,
   ViewStyle,
   KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
 
-const DismissKeyboardHOC = (
-  Comp: typeof KeyboardAvoidingView
-): React.FC<{ style?: StyleProp<ViewStyle> }> => {
-  return ({ children, ...props }) => (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <Comp {...props} style={props.style}>
-        {children}
-      </Comp>
-    </TouchableWithoutFeedback>
-  );
-};
-const DismissKeyboardView = DismissKeyboardHOC(KeyboardAvoidingView);
+const DismissKeyboardView: React.FC<{ style: StyleProp<ViewStyle> }> = ({
+  children,
+  ...props
+}) => (
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+    {/*
+		<KeyboardAvoidingView {...props} style={props.style} behavior='position'>
+		안드로이드에서는 position / 아이폰에서는 padding 적용
+		아래 안드로이드, 아이폰 분기처리
+		*/}
+    <KeyboardAvoidingView
+      {...props}
+      style={props.style}
+      behavior={Platform.OS === 'android' ? undefined : 'padding'}
+    >
+      {children}
+    </KeyboardAvoidingView>
+  </TouchableWithoutFeedback>
+);
 
 export default DismissKeyboardView;
